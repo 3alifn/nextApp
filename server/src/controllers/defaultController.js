53cb13ff0b8d2c5272629bbd28a7952f51378785
3alifn <3alifn30@gmail.com>
2025-02-27 21:53:57 +0600

    update api system

--- server/src/controllers/defaultController.js
+++ server/src/controllers/defaultController.js
@@ -1,5 +1,5 @@
 import { helloWorldModel } from "../models/defaultModel.js"
 export const helloWorldController = async (req, res)=>{
     const out= await helloWorldModel("Hello World")
-    res.status(200).json({out: out})
+    res.status(200).json(out);
 }
